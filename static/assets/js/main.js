583ecf0c27085b92b7ff68889722af191748f886
easa <eisanodehi@gmail.com>
2020-07-14 16:58:40 +0430

    manipulate the template

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -3,3 +3,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
     hljs.highlightBlock(block);
   })
 })
+
+var sc_project = 12358369;
+var sc_invisible = 0;
+var sc_security = "082aebef";
+var sc_https = 1;
+var scJsHost = "https://";
+document.write("<sc" + "ript type='text/javascript' src='" + scJsHost +
+  "statcounter.com/counter/counter.js'></" + "script>");
